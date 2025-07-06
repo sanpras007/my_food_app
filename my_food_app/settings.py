4c75523d37b8fcebf4e367f43141a88f4e95d62c
sanpras007 <sanpras2003@gmail.com>
2025-07-06 18:01:53 +0530

    added debug to false

--- my_food_app/settings.py
+++ my_food_app/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cnz_g219a2_sf1u=+1543^bcbyo*3lh!+byta)r2qu-soo))v7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'my_food_app.onrender.com','my-food-app-3xl6.onrender.com','13.51.234.234']
 
