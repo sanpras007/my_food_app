b72bc033057b49cc61ac1c8c67875fa7aa127e4f
sanpras007 <sanpras2003@gmail.com>
2025-07-28 23:10:47 +0530

    csrf trust created

--- my_food_app/settings.py
+++ my_food_app/settings.py
@@ -123,6 +123,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://officeexpensetracker.port0.org',
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
